5da1ae938438021c80a1415ab63a6e6d1df13593
S.Angelin Sukirtha <113704359+AngelinSukirtha@users.noreply.github.com>
2024-04-11 17:07:08 +0530

    Add files via upload

---
+++ CollegeDetails.java
@@ -0,0 +1,14 @@
+package com.chainsys.clgd.day1;
+
+public class CollegeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("College Name:AAA College of Engineering and Technology");
+    System.out.println("Department:Electronics and Communication Engineering");
+    System.out.println("Roll Number:953719106001");
+    System.out.println("Place:Sivakasi");
+    System.out.println("University:Anna University");
+	}
+
+}
