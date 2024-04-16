d00d21033b0cb93e6f89fd85fbcf766126c67c0c
ange3577 <ange3577@CSL0692.global.chainsys.com>
2024-04-16 16:05:23 +0530

    Challenges 1,2,3,4

---
+++ eclipse-workspace/CodingChallenges/src/com/chainsys/codingchallenge/Challenge4.java
@@ -0,0 +1,21 @@
+package com.chainsys.codingchallenge;
+
+import java.util.Scanner;
+
+public class Challenge4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		double d = sc.nextDouble();
+		sc.nextLine();
+		String s = sc.nextLine();
+		// Write your code here.
+		System.out.println("String: " + s);
+		System.out.println("Double: " + d);
+		System.out.println("Int: " + i);
+		sc.close();
+	}
+
+}
